ba8e9ee9b7dfbc563a5ff84b1fd96b4d713faab8
Shouvick <deeprony7@gmail.com>
2021-06-22 01:20:26 +0530

    query by partiton key only queality supported

---
+++ DynamoDB/MoviesQuery1.py
@@ -0,0 +1,32 @@
+import decimal
+import json
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+
+class DecimalEncoder(json.JSONEncoder):
+    '''Helper class to convert a DynamoDB item to JSON'''
+
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('Movies')
+
+year = 1970
+print(f"Movies from {year}")
+
+response = table.query(
+	KeyConditionExpression=Key('year').eq(year)
+)
+
+for movie in response['Items']:
+	print(f"{movie['year']} :\t{movie['title']}")
